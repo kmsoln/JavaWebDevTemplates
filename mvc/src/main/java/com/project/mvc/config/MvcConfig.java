aec4d7a5320215bdbc5412b4e4559ed61781b59f
kmsoln <kmsoln@vk.com>
2024-02-17 05:07:21 +0300

    Added all needed paths

--- mvc/src/main/java/com/project/mvc/config/MvcConfig.java
+++ mvc/src/main/java/com/project/mvc/config/MvcConfig.java
@@ -5,9 +5,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
-
+    
     public void addViewControllers(ViewControllerRegistry registry) {
+        // Home
+        registry.addRedirectViewController("/", "/home/index");
+        registry.addRedirectViewController("/home", "/home/index");
+        registry.addViewController("/home/index").setViewName("/home/index");
+        registry.addViewController("/home/dashboard").setViewName("/home/dashboard");
 
+        // Account
+        registry.addViewController("/login").setViewName("/account/login");
+        registry.addViewController("/signup").setViewName("");
     }
 
 }
