c76197b535f2230bbb67e25fbf20c75519e1832a
kmsoln <kmsoln@vk.com>
2024-02-18 15:19:42 +0300

    Cleared SignupModel

--- mvc/src/main/java/com/project/mvc/models/SignupModel.java
+++ mvc/src/main/java/com/project/mvc/models/SignupModel.java
@@ -15,33 +15,13 @@ public class SignupModel {
     @NotBlank(message = "Confirm Password is required")
     private String confirmPassword;
 
-
-    public SignupModel() {
-        // default constructor
-    }
-
     // Getters and setters
-    public String getUsername() {
-        return username;
-    }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
+    public String getConfirmPassword() { return confirmPassword; }
+    public void setConfirmPassword(String confirmPassword) { this.confirmPassword = confirmPassword; }
 }
