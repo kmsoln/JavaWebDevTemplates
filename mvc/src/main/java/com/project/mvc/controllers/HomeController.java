5e4d82904bc9fa7761d056e374b8f5330fdfa650
kmsoln <kmsoln@vk.com>
2024-02-19 00:29:03 +0300

    Changed HomeController to redirect authenticated user to dashboard instead of showing the home page for non-authenticated user.

--- mvc/src/main/java/com/project/mvc/controllers/HomeController.java
+++ mvc/src/main/java/com/project/mvc/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.project.mvc.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/home")
 public class HomeController {
     @GetMapping("/index")
-    public String homeView(Model model){
+    public String homeView(Model model, Authentication authentication){
+        if (authentication != null && authentication.isAuthenticated()) {
+            // User is authenticated, return a different view path
+            return "redirect:/home/dashboard";
+        }
+
+        // User is not authenticated, return the default view path
         model.addAttribute("viewPath", "home/index");
         return "shared/_layout";
     }
+
 
     @GetMapping("/dashboard")
     public String dashboardView(Model model){
